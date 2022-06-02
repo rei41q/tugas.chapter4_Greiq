961b08d1efe0958894af329c796794eae4068ca7
Rei Greiq <greiqrei@gmail.com>
2022-06-02 11:20:44 +0700

    update all

--- tugas1/javas.js
+++ tugas1/javas.js
@@ -1,67 +1,35 @@
-
-// const pargraf1 = document.getElementById("p1");
-// pargraf1.innerText = "hei there";
-// pargraf1.style.backgroundColor = 'orange';
-
-// const pargraf2 = document.getElementsByName("p2");
-// pargraf2[0].style.backgroundColor = 'blue';
-// pargraf2[1].style.backgroundColor = 'yellow';
-
-// let button1 = document.getElementById("button1");
-// let paragraf = document.getElementById("p1");
-// let color;
-
-// button1.onclick = function () {
-//   alert("button 1 clicked");
-//   if (color === "yellow") {
-//     paragraf.style.backgroundColor = "white";
-//     color = 'white';
-//   } else {
-//     paragraf.style.backgroundColor = "yellow";
-//     color = 'yellow';
-//   }
-//   //   paragraf.style.backgroundColor = "yellow";
-// };
-
-// let button2 = document.getElementById("button2");
-// button2.addEventListener("click", () => {
-//   alert("button 2 clicked");
-//   paragraf.style.backgroundColor = "red";
-// });
-let idnambah = 1;
-let button = document.getElementById("add");
-// let paragraf1 = document.getElementById("wrapper").getElementsByClassName("c1")[idtag];
-
-let tagsementara = document.getElementById("p1").id;
-let paragraf1 = document.getElementById("p1");
-// let newElement = document.createElement("div");
-// newElement.innerText = "New Element";
-let parent = document.getElementById("wrapper");
-
-button.onclick = function () 
-{
-
-let clonedElement = paragraf1.cloneNode(true);
-
-
-idnambah++;
-clonedElement.setAttribute('id', "p"+idnambah);
-clonedElement.setAttribute('class', "post"+idnambah);
-parent.append(clonedElement);
-// parent[0][1] = document.getElementById(tagsementara).id = idnambah;
-// document.getElementById(tagsementara).id = idnambah;
-// }
-
-
-
-// document.getElementById(tagsementara-1).id = idnambah+1;
-console.log("tag sementara",tagsementara);
-tagsementara = parseInt(tagsementara)+ 1;
-
-
-//   let clonedElement = parent.append(newElement);
+let klik1 = document.getElementById("buttonwarna");
+let klik2 = document.getElementById("buttondelete");
+let klik3 = document.getElementById("buttonnambah");
+let klik4 = document.getElementById("buttondeleteall");
+let post11 = document.getElementById("post1");
+let parent = document.getElementById("all post");
+let tagpos = 1;
+klik1.onclick = function(){
+    if( document.getElementById("post" + tagpos).style.backgroundColor === "white"){
+    document.getElementById("post" + tagpos).setAttribute
+    ("style"
+    ,"background-color: blue;color:white");
 }
+    else{
+        document.getElementById("post" + tagpos).setAttribute("style","background-color: white");
+    }
+    }
+    klik2.onclick = function(){
+        // parent.innerHTML = null;
+        document.getElementById("post" + tagpos).remove();
+        tagpos--;
+    }
 
-
-;
-
+    klik3.onclick = function(){
+        let clonenya = post11.cloneNode(true);
+        tagpos++;
+        clonenya.setAttribute("id", "post" + tagpos)
+        clonenya.innerHTML = "Text data " + tagpos;
+        clonenya.setAttribute('class', "cpost"+tagpos);
+        parent.append(clonenya);
+    }
+    klik4.onclick = function(){
+        parent.innerHTML = null;
+        tagpos = 0;
+    }
